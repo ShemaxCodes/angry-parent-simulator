88c7765b345e3a95abf398c9f15fbd72f59a3223
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2022-02-24 14:12:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 
+document.querySelector('#yell').addEventListener('click', run)
+
+
+function run() {
+    const fName = document.querySelector('#firstName').value
+    const fMid = document.querySelector('#firstMiddle').value
+    const lMid = document.querySelector('#middleName').value
+    const lName = document.querySelector('#lastName').value
+
+    document.querySelector('#placeToSee').innerText = `${fName} 
+    ${fMid} ${lMid} ${lName}`
+}
+
+
+
+
+// const synth = window.speechSynthesis; //Allows values to be read
+// document.querySelector('#yell').addEventListener('click', run)
+
+
+// function run() {
+//     const fName = document.querySelector('#firstName').value
+//     const fMid = document.querySelector('#firstMiddle').value
+//     const lMid = document.querySelector('#middleName').value
+//     const lName = document.querySelector('#lastName').value
+
+//     const yellText = `${fName} ${fMid} ${lMid} ${lName}`
+
+
+//     document.querySelector('#placeToSee').innerText = yellText;
+
+//     let yellThis = new SpeechSynthesisUtterance(yellText);
+
+//     synth.speak(yellThis);
+// }
+
