2769ee61b6e259c0bba3ffb0dd0edf977be98a08
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2022-02-24 14:23:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ document.querySelector('#yell').addEventListener('click', run)
 
 
 function run() {
-    const fName = document.querySelector('#firstName').value
-    const fMid = document.querySelector('#firstMiddle').value
-    const lMid = document.querySelector('#middleName').value
-    const lName = document.querySelector('#lastName').value
+    const fName = document.querySelector('#firstName').value.toUpperCase()
+    const fMid = document.querySelector('#firstMiddle').value.toUpperCase()
+    const lMid = document.querySelector('#middleName').value.toUpperCase()
+    const lName = document.querySelector('#lastName').value.toUpperCase()
 
     document.querySelector('#placeToSee').innerText = `${fName} 
-    ${fMid} ${lMid} ${lName}`
+    ${fMid} ${lMid} ${lName} !!!`
 }
 
 
