ec4e35c878e5f17fa76530444280a1d958c44344
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2022-02-25 10:44:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,37 +2,37 @@
 document.querySelector('#yell').addEventListener('click', run)
 
 
-function run() {
-    const fName = document.querySelector('#firstName').value.toUpperCase()
-    const fMid = document.querySelector('#firstMiddle').value.toUpperCase()
-    const lMid = document.querySelector('#middleName').value.toUpperCase()
-    const lName = document.querySelector('#lastName').value.toUpperCase()
-
-    document.querySelector('#placeToSee').innerText = `${fName} 
-    ${fMid} ${lMid} ${lName} !!!`
-}
-
-
-
-
-
-// const synth = window.speechSynthesis; //Allows values to be read
-// document.querySelector('#yell').addEventListener('click', run)
-
-
 // function run() {
-//     const fName = document.querySelector('#firstName').value
-//     const fMid = document.querySelector('#firstMiddle').value
-//     const lMid = document.querySelector('#middleName').value
-//     const lName = document.querySelector('#lastName').value
+//     const fName = document.querySelector('#firstName').value.toUpperCase()
+//     const fMid = document.querySelector('#firstMiddle').value.toUpperCase()
+//     const lMid = document.querySelector('#middleName').value.toUpperCase()
+//     const lName = document.querySelector('#lastName').value.toUpperCase()
 
-//     const yellText = `${fName} ${fMid} ${lMid} ${lName}`
-
-
-//     document.querySelector('#placeToSee').innerText = yellText;
-
-//     let yellThis = new SpeechSynthesisUtterance(yellText);
-
-//     synth.speak(yellThis);
+//     document.querySelector('#placeToSee').innerText = `${fName} 
+//     ${fMid} ${lMid} ${lName} !!!`
 // }
+
+
+
+
+
+const synth = window.speechSynthesis; //Allows values to be read
+document.querySelector('#yell').addEventListener('click', run)
+
+
+function run() {
+    const fName = document.querySelector('#firstName').value
+    const fMid = document.querySelector('#firstMiddle').value
+    const lMid = document.querySelector('#middleName').value
+    const lName = document.querySelector('#lastName').value
+
+    const yellText = `${fName} ${fMid} ${lMid} ${lName}`
+
+
+    document.querySelector('#placeToSee').innerText = yellText;
+
+    let yellThis = new SpeechSynthesisUtterance(yellText);
+
+    synth.speak(yellThis);
+}
 
